a095b72e052854a82e3164f401997f7339821dd5
Dane Edwards <Daneedw@gmail.com>
2023-08-25 10:33:45 -0400

    drills

---
+++ 12-SQL/01-Activities/29-SQL-Drills/drills.sql
@@ -0,0 +1,64 @@
+-- get everything from the employees table
+
+-- this time only retrieve the city, phone, and state, and officeCode
+
+
+-- filter the last query get all the officeCodes less than 3
+
+
+
+-- get everything from the offices table
+
+
+-- this time only retrieve the firstName, lastName and officeCode
+
+-- filter the last query, get the state of "CA"
+
+
+
+
+
+
+--join the offices table to the employees table and return all of the data
+
+
+--join the offices table to the employees table and return  firstName, lastName, officeCode city, phone, and state
+
+
+--filter the last query by city, only retrieve the entry for Boston
+
+
+--filter instead by getting all cities that contain the letter "o" in the city column
+
+
+
+
+
+
+
+--return everything from employees and customers
+
+
+
+-- join employee to customers table,
+-- return only the employee firstname and lastname, and customer firstname and last name and customer phone number
+
+-- return all the employees who do not have a customer
+
+-- return all the customers who do not have an employee
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
