ad350cc6b1d6652bd8132b39bc660c7ea42617aa
Dane Edwards <Daneedw@gmail.com>
2023-08-25 13:32:27 -0400

    added solutions

---
+++ 12-SQL/01-Activities/28-Stu_Mini-Project/index.mjs
@@ -0,0 +1,20 @@
+import inquirer from 'inquirer';
+
+
+try {
+    let answers = await inquirer
+    .prompt([
+      /* Pass your questions in here */
+    ])
+
+    if(answers.selectedChoice ==="get employee data"){
+
+
+        
+    }
+      
+} catch (error) {
+    
+}
+
+ 
